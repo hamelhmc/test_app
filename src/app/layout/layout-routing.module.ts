3504823d4f07c5fd55ce1f26f2daf327ce6ed58b
Hamilton Mercado Cuellar <hamelshmc@gmail.com>
2023-09-21 12:40:52 +0200

    ⭐️ feat: Agregamos Layout

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseLayoutComponent } from './components/base-layout/base-layout.component';
+import { NotFoundViewComponent } from './components/not-found-view/not-found-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BaseLayoutComponent,
+    children: [],
+  },
+  { path: '**', component: NotFoundViewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { BaseLayoutComponent } from './components/base-layout/base-layout.component';
+import { NotFoundViewComponent } from './components/not-found-view/not-found-view.component';
 import { LayoutRoutingModule } from './layout-routing.module';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [BaseLayoutComponent, NotFoundViewComponent, HeaderComponent],
   imports: [CommonModule, LayoutRoutingModule],
 })
 export class LayoutModule {}
